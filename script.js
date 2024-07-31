047fb1a7f66df78d4a00f7c61054f1406d569363
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-07-30 22:20:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const score = document.getElementById('score');
+const divEndGame = document.getElementById('endGame');
+const textVictory = document.getElementById('textVictory');
+let pointsPlayer1 = 0;
+let pointsPlayer2 = 0;
 
+const tickSpeed = 40;
 
 const heightRacket = 90;
 const widthRacket = 15;
@@ -19,7 +25,7 @@ let loopId;
 
 let directionBall;
 
-let chooseSide = () => {
+let chooseDirectionBall = () => {
     let side = Math.round(Math.random());
     if (side == 1) {
         directionBall = -90;
@@ -38,51 +44,52 @@ const drawSprites = () => {
 
     ctx.fillRect(positionXBall, //position X
         positionYBall,// position Y
-        sizeBall, sizeBall)
+        sizeBall, sizeBall);
     //width   //height
 }
 const drawGrid = () => {
     ctx.lineWidth = 1
     ctx.strokeStyle = 'gray';
 
-    for (let i = 0; i < canvas.width + 30; i += 30) {//linhas verticais
+    for (let i = 0; i < canvas.width + widthRacket*2; i += widthRacket*2) {//linhas verticais
         ctx.beginPath();
         ctx.lineTo(i, 0)
-        ctx.lineTo(i, 900)
+        ctx.lineTo(i, canvas.width)
         ctx.stroke()
 
         ctx.beginPath();
         ctx.lineTo(0, i);
-        ctx.lineTo(900, i);
+        ctx.lineTo(canvas.width, i);
         ctx.stroke();
     }
 }
-const speedBall = 30
+const geralSpeed = 30;
+const minusSpeed = 15;//to the diagonals
 const moveBall = () => {
     switch (directionBall) {
         case 45:
-            positionXBall += speedBall;//move right up
-            positionYBall -= speedBall;
+            positionXBall += minusSpeed;//move right up
+            positionYBall -= minusSpeed;
 
             break;
         case 90:
-            positionXBall += speedBall;//move only right
+            positionXBall += geralSpeed;//move only right
             break;
         case 135:
-            positionXBall += speedBall;
-            positionYBall += speedBall; //move right down
+            positionXBall += minusSpeed;
+            positionYBall += minusSpeed; //move right down
             break;
 
         case -45:
-            positionXBall -= speedBall;
-            positionYBall -= speedBall; //move up left
+            positionXBall -= minusSpeed;
+            positionYBall -= minusSpeed; //move up left
             break;
         case -90:
-            positionXBall -= speedBall; //move only left
+            positionXBall -= geralSpeed; //move only left
             break;
         case -135:
-            positionXBall -= 30; // move left down
-            positionYBall += 30;
+            positionXBall -= minusSpeed; // move left down
+            positionYBall += minusSpeed;
             break;
 
     }
@@ -111,41 +118,49 @@ const ballRacket = () => {
         directionBall = 135;
     }
     //racket 2
-    if ((positionXRacket2 - 30 == positionXBall) && (firstSquareRacket2 == positionYBall)) {
-        directionBall = -135;
-    }
-    if ((positionXRacket2 - 30 == positionXBall) && (secondSquareRacket2 == positionYBall)) {
-        directionBall = -90;
-    }
-    if ((positionXRacket2 - 30 == positionXBall) && (thirdSquareRacket2 == positionYBall)) {
+    if ((positionXRacket2 - widthRacket * 2 == positionXBall) && (firstSquareRacket2 == positionYBall)) {
         directionBall = -45;
     }
+    if ((positionXRacket2 - widthRacket * 2 == positionXBall) && (secondSquareRacket2 == positionYBall)) {
+        directionBall = -90;
+    }
+    if ((positionXRacket2 - widthRacket * 2 == positionXBall) && (thirdSquareRacket2 == positionYBall)) {
+        directionBall = -135;
+    }
+}
+const resetPositions = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    positionYRacket1 = canvas.height / 2 - heightRacket / 2;
+    positionYRacket2 = canvas.height / 2 - heightRacket / 2;
+    positionXBall = canvas.width / 2 - sizeBall / 2;
+    positionYBall = canvas.height / 2 - sizeBall / 2;
 }
 const ballWall = () => {
-    const resetPositions=()=>{
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        positionYRacket1 = canvas.height / 2 - heightRacket / 2;
-        positionYRacket2 = canvas.height / 2 - heightRacket / 2;
-        positionXBall = canvas.width / 2 - sizeBall / 2;
-        positionYBall = canvas.height / 2 - sizeBall / 2;
-        
-        game();
-    }
-    if (positionXBall == -30) { // left and right walls
+    if (positionXBall == - sizeBall) { // left walls
         directionBall = 90;
+        pointsPlayer2++;
         resetPositions();
     }
-    if(positionXBall == canvas.width){
+    if (positionXBall == canvas.width) {// right walls
         directionBall = -90;
+        pointsPlayer1++;
         resetPositions();
     }
-    if (positionYBall == 0 || positionYBall == canvas.height-30) { //up and down walls
+    if (positionYBall == 0 || positionYBall == canvas.height - sizeBall) { //up and down walls
         if (directionBall == 45 || directionBall == -135) {
             directionBall += 90;
 
         } else {
             directionBall -= 90;
         }
+    }
+}
+const endGame = () => {
+    divEndGame.style.visibility = "visible";
+    if(pointsPlayer1==3){
+        textVictory.innerText = `O player 1 venceu!`;
+    }else{
+        textVictory.innerText = `O player 2 venceu!`;
     }
 }
 const game = () => {
@@ -158,18 +173,29 @@ const game = () => {
     ballRacket();
     ballWall();
     drawGrid();
+
+    score.innerText= `${pointsPlayer1}  |  ${pointsPlayer2}`;
+
     loopId = setTimeout(() => {
-        game();
-    }, 200)
+        
+        if(!(pointsPlayer1 == 3 || pointsPlayer2 == 3)){
+            game();
+        }else{
+            endGame();
+        }
+    }, tickSpeed)
+}
+const restart = () => {
+    window.location.reload();
 }
 game();
-chooseSide();
+chooseDirectionBall();
 
 document.addEventListener('keydown', function (tecla) {//racket 1
     if (!(positionYRacket1 <= 5)) {//arrow up
         switch (tecla.keyCode) {
             case 87:
-                positionYRacket1 -= 30;
+                positionYRacket1 -= geralSpeed;
                 break;
         }
     }
@@ -177,7 +203,7 @@ document.addEventListener('keydown', function (tecla) {//racket 1
 
         switch (tecla.keyCode) {
             case 83:
-                positionYRacket1 += 30;
+                positionYRacket1 += geralSpeed;
                 break;
         }
     }
@@ -186,7 +212,7 @@ document.addEventListener('keydown', function (tecla) {//racket 2
     if (!(positionYRacket2 <= 5)) {//arrow up
         switch (tecla.keyCode) {
             case 38:
-                positionYRacket2 -= 30;
+                positionYRacket2 -= geralSpeed;
                 break;
         }
     }
@@ -194,7 +220,7 @@ document.addEventListener('keydown', function (tecla) {//racket 2
 
         switch (tecla.keyCode) {
             case 40:
-                positionYRacket2 += 30;
+                positionYRacket2 += geralSpeed;
                 break;
         }
     }
